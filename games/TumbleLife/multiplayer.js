6f1a99568ecb50df8ea20f0cfeb563a038a38423
sortofdevofficial <sortofdevofficial@gmail.com>
2026-06-10 13:30:06 +0530

    Update multiplayer.js

--- games/TumbleLife/multiplayer.js
+++ games/TumbleLife/multiplayer.js
@@ -1,1 +1,6 @@
-
+export function makeCode(len = 2) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let out = '';
+  for (let i = 0; i < len; i++) out += chars[(Math.random() * chars.length) | 0];
+  return out;
+}
